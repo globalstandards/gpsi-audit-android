6d090e6cf2358d992996336dc45219334c2409a4
GVAZQUEZ\Gabriel Vazquez <gabriel.vazquezruiz>
2017-03-22 17:28:06 -0600

    Revert "detectar wifi en login"
    This reverts commit 2e5148556dae52603158ff7db14e563f2ee3d9b2.

--- app/src/main/java/globalstd/globalaudit/activitys/LoginActivity.java
+++ app/src/main/java/globalstd/globalaudit/activitys/LoginActivity.java
@@ -2,10 +2,7 @@ package globalstd.globalaudit.activitys;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
@@ -45,16 +42,11 @@ public class LoginActivity extends BaseActivity {
     RelativeLayout btnLogin;
     RelativeLayout btnNewAcount;
 
-    public static boolean isOnline=true;
-    Snackbar snackbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
-        validateInternet();
-
 
         txtEmail = (TextInputEditText) findViewById(R.id.txtEmail);
         txtPsw = (TextInputEditText) findViewById(R.id.txtPsw);
@@ -66,23 +58,22 @@ public class LoginActivity extends BaseActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isOnline) {
-                    hideKeyboard();
+                hideKeyboard();
 
                 /*if (!validateEmail(txtEmail.getText().toString())) {
                     txtEmail.setError(getString(R.string.invalidad_email));
                 } else*/
-                    if (!validatePassword(txtPsw.getText().toString())) {
-                        txtPsw.setError(getString(R.string.enter_psw));
-                    } else {
-                        txtEmail.setEnabled(false);
-                        txtPsw.setEnabled(false);
+                if (!validatePassword(txtPsw.getText().toString())) {
+                    txtPsw.setError(getString(R.string.enter_psw));
+                } else {
+                    txtEmail.setEnabled(false);
+                    txtPsw.setEnabled(false);
 
-                        btnLogin.setEnabled(false);
-                        btnNewAcount.setEnabled(false);
-                        eventBus.post(new SignInEvent(txtEmail.getText().toString(), txtPsw.getText().toString()));
-                    }
+                    btnLogin.setEnabled(false);
+                    btnNewAcount.setEnabled(false);
+                    eventBus.post(new SignInEvent(txtEmail.getText().toString(), txtPsw.getText().toString()));
                 }
+
 
 
             }
@@ -124,7 +115,7 @@ public class LoginActivity extends BaseActivity {
         if (event.error != null) {
             switch (event.error.getCode()) {
                 case GlobalAuditException.INVALID_CREDENTIALS:
-                    snackbar = Snackbar.make(coordinatorLayout, getResources().getString(R.string.invalid_credentials), Snackbar.LENGTH_LONG);
+                    Snackbar snackbar = Snackbar.make(coordinatorLayout, getResources().getString(R.string.invalid_credentials), Snackbar.LENGTH_LONG);
                     snackbar.show();
                     desbloqueo();
                     break;
@@ -181,139 +172,6 @@ public class LoginActivity extends BaseActivity {
         txtPsw.setEnabled(true);
         btnLogin.setEnabled(true);
         btnNewAcount.setEnabled(true);
-    }
-
-
-    public void validateInternet(){
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                //Toast.makeText(getApplication(), "Testing timer", Toast.LENGTH_SHORT).show();
-                if(isConnected()){
-                    if(isOnline==false) {
-                        isConnectionReset();
-                    }
-                }
-                handler.postDelayed(this, 15000); //now is every 2 minutes
-            }
-        }, 15000); //Every 120000 ms (2 minutes)
-        // comprobar si esta conectado a internet
-    }
-    public Boolean isConnected(){
-        if(isOnlineWifiNetwork()){
-            //showAlertDialog(this, "Conexion a Internet", "Tu Dispositivo tiene Conexion a Wifi.", true);
-            return true;
-        }else{
-            if(isOnlineMobileNetwork()){
-                //showAlertDialog(this, "Conexion a Internet", "Tu Dispositivo tiene Conexion Movil.", true);
-                return true;
-            }else{
-                isOnline=false;
-                showAlertDialog(this, "Conexion a Internet", "Tu Dispositivo no tiene Conexion a Internet.", false);
-                return false;
-            }
-        }
-    }
-    public Boolean isConnectionReset(){
-        if(isOnlineWifiNetwork()){
-            isOnline=true;
-            showAlertDialogTemp(this, "Conexion a Internet",
-                    "Tu Dispositivo tiene Conexion a Wifi.", true);
-            return true;
-        }else{
-            if(isOnlineMobileNetwork()){
-                isOnline=true;
-                showAlertDialogTemp(this, "Conexion a Internet",
-                        "Tu Dispositivo tiene Conexion Movil.", true);
-                return true;
-            }else{
-                isOnline=false;
-                showAlertDialog(this, "Conexion a Internet",
-                        "Tu Dispositivo no tiene Conexion a Internet.", false);
-                return false;
-            }
-        }
-    }
-
-    protected Boolean isOnlineWifiNetwork(){
-        ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivity != null) {
-            NetworkInfo info = connectivity.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-            if (info != null) {
-                if (info.isConnected()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    protected Boolean isOnlineMobileNetwork(){
-        ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivity != null) {
-            NetworkInfo info = connectivity.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-            if (info != null) {
-                if (info.isConnected()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    public void showAlertDialog(Context context, String title, String message, Boolean status) {
-
-        snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_INDEFINITE);
-        //snackbar.setActionTextColor(this.getResources().getColor(R.color.colorPrimary));
-
-        View view = snackbar.getView();
-        //view.setBackgroundColor(this.getResources().getColor(R.color.white));
-
-        //Get the textview of the snackbar text
-        //TextView textView = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
-
-        //set text color
-        //textView.setTextColor(this.getResources().getColor(R.color.colorAccent));
-
-        /// /increase max lines of text in snackbar. default is 2.
-        //textView.setMaxLines(10);
-
-        // Position
-        /*
-        CoordinatorLayout.LayoutParams params=(CoordinatorLayout.LayoutParams)view.getLayoutParams();
-        params.gravity = Gravity.TOP;
-        view.setLayoutParams(params);
-        */
-        snackbar.show();
-
-
-
-    }
-    public void showAlertDialogTemp(Context context, String title, String message, Boolean status) {
-
-        snackbar = Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT);
-        //snackbar.setActionTextColor(this.getResources().getColor(R.color.colorPrimary));
-
-        View view = snackbar.getView();
-        view.setBackgroundColor(this.getResources().getColor(R.color.strip_1));
-
-        //Get the textview of the snackbar text
-        //TextView textView = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
-
-        //set text color
-        //textView.setTextColor(this.getResources().getColor(R.color.colorAccent));
-
-        /// /increase max lines of text in snackbar. default is 2.
-        //textView.setMaxLines(10);
-
-        // Position
-        /*
-        CoordinatorLayout.LayoutParams params=(CoordinatorLayout.LayoutParams)view.getLayoutParams();
-        params.gravity = Gravity.TOP;
-        view.setLayoutParams(params);
-        */
-        snackbar.show();
-
-
-
     }
 
 }
